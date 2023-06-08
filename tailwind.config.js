151d09e26ec26278758a963da59ff7c7cf89b599
HydroOx1d <n-esenov44@mail.ru>
2023-06-08 23:51:35 +0600

    custom color tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      black: '#111111',
+      white: '#F9F9F9',
+      darkGray: '#161616',
+      orange: '#FF7425',
+      silver: '#C9C9C9',
+      black2: '#222222',
+      lightGray: '#F5F5F5',
+    },
     extend: {},
   },
   plugins: [],
