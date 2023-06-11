8e03585b65b00dd252d19461377419811345002d
HydroOx1d <n-esenov44@mail.ru>
2023-06-11 12:00:23 +0600

    adaptive footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="footer py-8">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-4">
-          <div className={`logo ${jetBrainsMono.className} text-3xl mr-16`}>
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          <div className={`logo ${jetBrainsMono.className} text-3xl mr-16 col-span-4 sm:col-span-4 lg:col-span-1`}>
             <span>DF</span>
             <span className="text-orange">A</span>
           </div>
@@ -36,7 +36,7 @@ function Footer() {
               История покупок
             </Link>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col col-span-4 sm:col-span-3 md:col-span-1">
             <Link
               href="/"
               className={`hover:text-orange transition-colors ${jetBrainsMono.className}`}
@@ -71,8 +71,8 @@ function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex justify-between mt-28">
-          <span>© ОсОО “DUTY FREE ALTYN”</span>
+        <div className="flex justify-between mt-28 sm:flex-row flex-col">
+          <span className="mb-4 sm:mb-0">© ОсОО “DUTY FREE ALTYN”</span>
           <span className="uppercase">Все права защищены</span>
         </div>
       </div>
